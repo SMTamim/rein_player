30f6237d649a3dca21826ef046ed4c2a10f3807a
Ahurein <ahureinebenezer@gmail.com>
2025-01-13 11:51:04 +0000

    feat: display timestamps

--- lib/utils/constants/rp_text.dart
+++ lib/utils/constants/rp_text.dart
@@ -2,4 +2,7 @@ class RpText {
   RpText._();
 
   static const appName = "Rein Player";
+
+  static const defaultVideoDuration = "00:00";
+  static const defaultVideoTimeWatched = "00:00";
 }
---
+++ lib/utils/helpers/duration_helper.dart
@@ -0,0 +1,15 @@
+class RpDurationHelper {
+
+  static String formatDuration(Duration duration) {
+    String twoDigits(int n) => n.toString().padLeft(2, '0');
+    final hours = duration.inHours;
+    final minutes = duration.inMinutes % 60;
+    final seconds = duration.inSeconds % 60;
+
+    if (hours > 0) {
+      return "${twoDigits(hours)}:${twoDigits(minutes)}:${twoDigits(seconds)}";
+    } else {
+      return "${twoDigits(minutes)}:${twoDigits(seconds)}";
+    }
+  }
+}
